e21ea403bebfb57858e01a20ec877594ca3eff4e
uzomao <chidumaga@gmail.com>
2024-07-02 17:44:06 +0100

    adds handling for browser caching

--- data/fetchContent.js
+++ data/fetchContent.js
@@ -1,15 +1,18 @@
 const headers = {
-    "Authorization": `bearer ${process.env.STRAPI_ACCESS_TOKEN}`
+    "Authorization": `bearer ${process.env.STRAPI_ACCESS_TOKEN}`,
+    "Cache-Control": "no-store, no-cache, must-revalidate, proxy-revalidate, max-age=0",
+    "Pragma": "no-cache",
+    "Expires": "0"
 }
 
 const getData = async (endpoint) => {
-    const response = await fetch(`${process.env.STRAPI_URL}/api/${endpoint}?populate=*`, {headers})
+    const response = await fetch(`${process.env.STRAPI_URL}/api/${endpoint}?populate=*`, {
+        headers,
+        cache: 'no-store'
+    })
     const data = await response.json()
     
     return data
 }
 
 export { getData }
-
-const pages = await getData('pages')
-console.log('PAGES: ', pages)
