d5812f97a7511b2d56e4c4a07ec9da60ddd547d1
uzomao <chidumaga@gmail.com>
2024-07-13 18:49:07 +0100

    much better 🙃

--- src/app/[slug]/page.js
+++ src/app/[slug]/page.js
@@ -1,25 +1,19 @@
-// Basically a clone of app/page.js
+// Utilises app/page.js for single page rendering also
 // For most/all themes, there is/should be conditional logic to determine whether to render the template layout or a specific page
 // By replicating app/page.js, we allow the possibility of using more conventional Next routing while also solving for the problem
 // of the single page template overriding the layout template (see comment in Notion board)
 
 import React from 'react'
+import Page from '../page'
 
-import ActiveTemplate from '../activeTemplate'
+const SinglePage = () => {
 
-import { appState } from '../layout'
-
-const Home = () => {
-
-  const { pages } = appState
-
-  // This is a server component which uses a client component for state mgt and interactivity:
   return (
     <>
-      { pages && <ActiveTemplate pages={pages} /> }
-      {/* We can do more stuff here like rendering the correct contentType, etc */}
+      <Page />
+      {/* more stuff here */}
     </>
   );
 }
 
-export default Home
+export default SinglePage
