8a0c14ef23090e81d095a1cca24b6ef2ec5bb5db
uzomao <chidumaga@gmail.com>
2025-04-04 20:08:59 +0100

    enhances starter theme

--- templates/starter/page.js
+++ templates/starter/page.js
@@ -1,15 +1,21 @@
 'use client'
 
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useContext } from 'react'
 import { StyledContainer } from './styles';
 import { renderCurrentPage } from '../../utils/utils';
 
 import Index from './layout/index'
 import SinglePage from './layout/single'
+import { AppContext } from '../../context'
 
 const Page = ({ pages }) => {
 
     const [currentPage, setCurrentPage] = useState(null)
+
+
+    // Get site metadata from React Context
+    const context = useContext(AppContext)
+    const { siteTitle, siteDescription } = context || {}
 
     useEffect(() => {
         setCurrentPage(renderCurrentPage(pages))
@@ -22,7 +28,8 @@ const Page = ({ pages }) => {
             {
                 !currentPage ?
                     <>
-                        <h1 style={{fontSize: '6rem', marginBottom: '2rem'}}>Portal Starter</h1>
+                        <h1 style={{fontSize: '6rem', marginBottom: '2rem'}}>{siteTitle ? siteTitle : 'Portal Starter'}</h1>
+                        {siteDescription && <p>{siteDescription}</p> }
                         <Index pages={pages} setCurrentPage={setCurrentPage} />
                     </>
                     :
