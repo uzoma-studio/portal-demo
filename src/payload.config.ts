9fc06e9b1dc25f61d0b3d2ac7fa2742e2b2f565f
uzomao <chidumaga@gmail.com>
2025-04-29 18:04:45 +0100

    updates to s3storage plugin

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,4 +1,6 @@
 // storage-adapter-import-placeholder
+//@ts-nocheck
+import { s3Storage } from '@payloadcms/storage-s3'
 import { postgresAdapter } from '@payloadcms/db-postgres'
 import { payloadCloudPlugin } from '@payloadcms/payload-cloud'
 import { lexicalEditor } from '@payloadcms/richtext-lexical'
@@ -6,7 +8,6 @@ import path from 'path'
 import { buildConfig } from 'payload'
 import { fileURLToPath } from 'url'
 import sharp from 'sharp'
-import { s3Storage } from '@payloadcms/storage-s3'
 
 import { Users } from './collections/Users'
 import { Media } from './collections/Media'
@@ -103,7 +104,6 @@ export default buildConfig({
   }),
   sharp,
   plugins: [
-    payloadCloudPlugin(),
     s3Storage({
       collections: {
         media: true,
@@ -111,13 +111,16 @@ export default buildConfig({
       bucket: process.env.S3_BUCKET,
       config: {
         credentials: {
+          forcePathStyle: true,
           accessKeyId: process.env.S3_ACCESS_KEY_ID,
           secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
         },
         endpoint: process.env.S3_ENDPOINT,
         region: 'auto',
+        clientUploads: true,
       },
-    })
+    }),
+    payloadCloudPlugin(),
     // storage-adapter-placeholder
   ],
 })
